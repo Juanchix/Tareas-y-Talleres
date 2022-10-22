fb0fdba64dad801eaad5719f034583939e8d539f
Juan Diego Ortega <j.ortegam@uniandes.edu.co>
2022-10-22 17:44:33 -0500

    intento de tutorial

---
+++ scripts/Taller inicial TS-JS/dataCourses.js
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW", "José", 3),
+    new Course("Lenguajes y Máquinas", "Nicolas", 3),
+    new Course("Cálculo Integral", "Andrey", 3),
+    new Course("Física", "Julian", 3),
+    new Course("Química", "Elizabeth", 3),
+    new Course("LAB. Química", "Sandra", 0),
+    new Course("CBU Colombia", "Carlos", 2)
+];
