950458acfa7d903a6295b4e578591b397865b0e7
Juan Diego Ortega <j.ortegam@uniandes.edu.co>
2022-10-30 00:27:24 -0500

    Tuto de angular 2.0

---
+++ Talleres Angular/tutorialCourses/src/app/course/dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from "./course";
+
+export const dataCourses = [
+  new Course("Ingeniería de software", "José", 3),
+  new Course("Lenguajes y Máquinas", "Nicolas", 3),
+  new Course("Cálculo Integral", "Andrey", 3),
+  new Course("Química", "Elizabeth", 3),
+  new Course("Física", "Julian", 3),
+];
